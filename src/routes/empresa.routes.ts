7919a94daf4b2d919b195ae748fa3020742e2e85
Jairo Júnior <jairojjr2003@gmail.com>
2025-06-10 14:33:43 -0300

    fix/feat: added the Nest.js to the project

--- src/routes/empresa.routes.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { criarEmpresa, listarEmpresas } from "../controllers/empresa.controller";
-
-const router = express.Router();
-
-router.post("/", criarEmpresa);
-router.get("/", listarEmpresas);
-
-export default router;
